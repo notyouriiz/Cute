13e39d707b9e203a2004ab2b0136c336f6cd9d07
notyouriiz <faizadhytia24@gmail.com>
2025-02-16 12:25:54 +0700

    modify code to deployable on replit

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ def write_message(name):
 turtle.speed(3)
 
 draw_heart()
-write_message("Aira")  # Replace with a specific name
+write_message("Aira") 
 
 turtle.hideturtle()
-turtle.done()
+
+if __name__ == "__main__":
+    turtle.done()
